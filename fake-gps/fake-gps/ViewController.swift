24ca077152179c6e6c8099cf31af9c039a3d40d5
Jianfu Zhang <jeff.zhang223@gmail.com>
2016-07-20 20:40:14 -0700

    fake gps init

---
+++ fake-gps/fake-gps/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  fake-gps
+//
+//  Created by Jianfu Zhang on 7/20/16.
+//  Copyright © 2016 Jianfu Zhang. All rights reserved.
+//
+
+import UIKit
+//import CoreLocation
+
+class ViewController: UIViewController {
+    
+//    var locationManager:CLLocationManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+//        locationManager = CLLocationManager()
+//        locationManager.delegate = self
+//        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        locationManager.requestAlwaysAuthorization()
+//        locationManager.startUpdatingLocation()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
